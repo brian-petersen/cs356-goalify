ef9e18b0c3e850776bb1530dd54bd512b8be5a0d
Brian Petersen <skiwalk3r@gmail.com>
2018-10-11 07:34:23 -0600

    Adds labels to checkboxes on overview

--- app/src/main/java/io/goalify/android/views/OverviewActivity.kt
+++ app/src/main/java/io/goalify/android/views/OverviewActivity.kt
@@ -10,6 +10,7 @@ import io.goalify.android.models.AppDatabase
 import io.goalify.android.models.Goal
 import io.goalify.android.viewmodels.OverviewViewModel
 import kotlinx.android.synthetic.main.layout_overview.*
+import java.util.*
 
 class OverviewActivity : AppCompatActivity() {
 
@@ -34,27 +35,76 @@ class OverviewActivity : AppCompatActivity() {
             adapter.notifyDataSetChanged()
         })
 
-        // Test Goals
-        AppDatabase.getInstance()?.goalDao()?.create(
-            Goal(
-                name = "Test 1",
-                question = "Test 1?",
-                frequency = 0,
-                reminderHourOfDay = 15,
-                reminderMinute = 0,
-                reminderFrequency = 0
-            )
+        setSummaryDayLabels()
+
+        createTestGoals(20)
+    }
+
+    private fun getSummaryDays(): List<Calendar> {
+        val cal = Calendar.getInstance()
+        cal.set(Calendar.HOUR_OF_DAY, 0)
+        cal.set(Calendar.MINUTE, 0)
+        cal.set(Calendar.SECOND, 0)
+
+        val days = mutableListOf<Calendar>()
+        days.add(cal.clone() as Calendar)
+        for (i in 1..4) {
+            cal.add(Calendar.DATE, 1)
+            days.add(cal.clone() as Calendar)
+        }
+
+        return days
+    }
+
+    private fun setSummaryDayLabels() {
+        val daysOfWeek = listOf(
+            "SUN",
+            "MON",
+            "TUE",
+            "WED",
+            "THU",
+            "FRI",
+            "SAT"
         )
 
-        AppDatabase.getInstance()?.goalDao()?.create(
-            Goal(
-                name = "Test 2",
-                question = "Test 2?",
-                frequency = 0,
-                reminderHourOfDay = 15,
-                reminderMinute = 0,
-                reminderFrequency = 0
-            )
+        val textViewWeekDays = listOf(
+            textViewDayOfWeek1,
+            textViewDayOfWeek2,
+            textViewDayOfWeek3,
+            textViewDayOfWeek4,
+            textViewDayOfWeek5
         )
+
+        val textViewDays = listOf(
+            textViewDay1,
+            textViewDay2,
+            textViewDay3,
+            textViewDay4,
+            textViewDay5
+        )
+
+        val days = getSummaryDays()
+
+        for (i in 0..4) {
+            textViewWeekDays[i].text = daysOfWeek[days[i].get(Calendar.DAY_OF_WEEK) - 1]
+            textViewDays[i].text = days[i].get(Calendar.DATE).toString()
+        }
+    }
+
+
+    @Suppress("unused")
+    private fun createTestGoals(count: Int) {
+        for (i in 1..count) {
+            AppDatabase.getInstance()?.goalDao()?.create(
+                Goal(
+                    name = "Test $i",
+                    question = "Test $i?",
+                    frequency = 0,
+                    reminderHourOfDay = 15,
+                    reminderMinute = 0,
+                    reminderFrequency = 0
+                )
+            )
+        }
     }
 }
